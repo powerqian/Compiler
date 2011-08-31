494afaa6a95e1097de187ddc3f52b4a338a5675b
PowerQian <powerqian0827@hotmail.com>
2011-08-31 23:02:22 +0800

    Initial Commit @ Aug 31

--- Compiler/CompilerAppDelegate.h
+++ Compiler/CompilerAppDelegate.h
@@ -7,11 +7,17 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "Lexer.h"
+#import "Parser.h"
 
 @interface CompilerAppDelegate : NSObject <NSApplicationDelegate> {
     NSWindow *window;
+    Lexer *myLexer;
+    Parser *myParser;
 }
 
 @property (assign) IBOutlet NSWindow *window;
+
+-(IBAction) test:(id)sender;
 
 @end
--- Compiler/CompilerAppDelegate.m
+++ Compiler/CompilerAppDelegate.m
@@ -15,6 +15,22 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    myLexer = [[Lexer alloc] init];
+    myParser = [[Parser alloc] init];
+    
+}
+
+-(IBAction) test:(id)sender
+{
+//    [myLexer loadText];
+//    [myLexer preProcessor];
+//    [myLexer lexicalAnalyze:10];
+//    [myParser loadLex:myLexer.result withSymbolTable:myLexer.table1];
+    [myParser start];
+    NSLog(@"The result:");
+    for (NSString *str in myParser.intermediateCode){
+        NSLog(@"%@",str);
+    }
 }
 
 @end
